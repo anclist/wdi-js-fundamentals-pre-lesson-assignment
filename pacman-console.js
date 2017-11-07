f11d48b47dfe3d19bc4577f3a849cf9d76bb1d31
Tony <ANCLIST@GMAIL.COM>
2017-11-07 00:40:31 -0500

    Lopps

--- pacman-console.js
+++ pacman-console.js
@@ -1,1 +1,5 @@
-console.log('Pac-Man eats ghosts for lunch.');
+var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+for (var index = 0; index < ghosts.length; index++) {
+  console.log('Pac-Man eats ' + ghosts[index]);
+}
